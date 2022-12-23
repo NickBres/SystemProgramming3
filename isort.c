c6c7a39f471ea71e24d6a045bd51ee7a8e704d89
NickBres <breslavsky97@gmail.com>
2022-12-23 13:47:30 +0200

    added comments

--- isort.c
+++ isort.c
@@ -2,13 +2,14 @@
 
 #define SIZE 100
 
-int *find_place(int* arr,int *pos); // find the place to insert the number
-void insertion_sort(int* arr , int len); // sort the array
-int get_arr(int *arr); // get the array from the user
-void print_array(int* arr, int len); // print the array
-void shift_element(int* arr, int i); // shift the array
+int *find_place(int *arr, int *pos);    // find the place to insert the number
+void insertion_sort(int *arr, int len); // sort the array
+int get_arr(int *arr);                  // get the array from the user
+void print_array(int *arr, int len);    // print the array
+void shift_element(int *arr, int i);    // shift the array
 
-int main(){
+int main()
+{
     int arr[SIZE] = {0};
     int len = get_arr(arr);
     insertion_sort(arr, len);
@@ -16,64 +17,71 @@ int main(){
     return 0;
 };
 
-void print_array(int* arr, int len){
-    int* p = arr;
-    int* r = arr + len;
+void print_array(int *arr, int len)
+{
+    int *p = arr;       // the first element
+    int *r = arr + len; // the last element
     printf("%d", *p++);
-    while (p != r){
+    while (p != r)
+    {
         printf(",%d", *p++);
     }
     printf("\n");
 };
 
-void shift_element(int* arr, int i){
-    int* p = arr + i;
-    int* q = arr + i - 1;
-    while (p != arr){
+void shift_element(int *arr, int i)
+{
+    int *p = arr + i;
+    int *q = arr + i - 1;
+    while (p != arr)
+    { // from the last element to the first element
         *p = *q;
         p--;
         q--;
     }
 };
 
-void insertion_sort(int* arr , int len){
-    int* p = arr;
-    int* q = arr + 1;
-    int* r = arr + len;
-    while (q != r){
-        if (*q < *p){
-            int* place = find_place(arr,q);
-            int num = *q;
-            shift_element(place, q - place);
-            *place = num;
-           // print_array(arr, len);
+void insertion_sort(int *arr, int len)
+{
+    int *p = arr;       // the first element
+    int *q = arr + 1;   // the second element
+    int *r = arr + len; // the last element
+    while (q != r)
+    {
+        if (*q < *p)
+        {                                    // if the second element is smaller than the first element
+            int *place = find_place(arr, q); // find the place to move it to
+            int num = *q;                    // save the number
+            shift_element(place, q - place); // shift the array
+            *place = num;                    // insert the number
         }
-        
         p++;
         q++;
     }
 };
 
-int *find_place(int* arr,int *pos){
-    int *p = pos;
-    int num = *p;
-    while(p >= arr && *p >= num){
+int *find_place(int *arr, int *pos)
+{
+    int *p = pos;                 // the position of the number
+    int num = *p;                 // the number
+    while (p >= arr && *p >= num) // while the number is smaller than the previous number
+    {
         p--;
     }
-    return p + 1;
+    return p + 1; // return the place to insert the number
 };
 
-int get_arr(int *arr){
-    int *p = arr;
-    int *q = arr + SIZE;
+int get_arr(int *arr)
+{
+    int *p = arr;        // the first element
+    int *q = arr + SIZE; // the last element
     int num;
-    int i = 0;
-    while (p != q && scanf("%d", &num) == 1){
+    int i = 0;                               // the number of elements
+    while (p != q && scanf("%d", &num) == 1) // get the array from the user
+    {
         *p = num;
         i++;
         p++;
     }
     return i;
 };
-
-
--- txtfind.c
+++ txtfind.c
@@ -4,13 +4,13 @@
 #define LINE 256
 #define WORD 30
 
-int getLine(char s[]);
-int getword(char *w);
-int substring(char *str1, char *str2);
-int similar(char *s, char *t, int n);
-void print_lines(char *str);
-void print_similar_words(char *str);
-void print(char *arr, int len);
+int getLine(char s[]);                 // get line from stdin char by char
+int getword(char *w);                  // get word from stdin char by char
+int substring(char *str1, char *str2); // check if str2 is substring of str1
+int similar(char *s, char *t, int n);  // check if s is similar to t with n changes
+void print_lines(char *str);           // print lines that contain str
+void print_similar_words(char *str);   // print words that are similar to str
+void print(char *arr);                 // print string
 
 int main()
 {
@@ -32,13 +32,13 @@ int main()
 void print_similar_words(char *str)
 {
     int length = 1;
-    while (length < WORD - 1)
+    while (length < WORD)
     {
         char word[WORD] = {0};
         length = getword(word);
         if (length >= (int)strlen(str) && similar(word, str, 1))
         {
-            print(word, length);
+            print(word);
         }
     }
 }
@@ -46,13 +46,13 @@ void print_similar_words(char *str)
 void print_lines(char *str)
 {
     int length = 1;
-    while (length < LINE - 1)
+    while (length < LINE)
     {
         char line[LINE] = {0};
         length = getLine(line);
         if (substring(line, str))
         {
-            print(line, length);
+            print(line);
         }
     }
 }
@@ -65,17 +65,17 @@ int similar(char *s, char *t, int n)
     int count = 0;
     while (p != pe)
     {
-        if (*p == *q)
+        if (*p == *q) // same char in both strings
         {
             p++;
             q++;
         }
-        else
+        else // different char
         {
             p++;
             count++;
         }
-        if (count > n)
+        if (count > n) // to much need to delete
             return 0;
     }
     return 1;
@@ -91,21 +91,21 @@ int substring(char *str1, char *str2)
     while (p != r)
     {
 
-        if (*p == *q)
+        if (*p == *q) // same char in both strings
         {
             q++;
             p++;
         }
-        else if (q == str2)
+        else if (q == str2) // q is at the start of str2 and chars are different
         {
             p++;
         }
-        else // return q to the start of str2
+        else // chars are different, return q to the start of str2
         {
             q = str2;
         }
 
-        if (q == s)
+        if (q == s) // all chars in str2 were found in str1
             return 1;
     }
     return 0;
@@ -116,12 +116,12 @@ int getword(char w[])
     char *p = w;
     int charCount = 0;
     char c;
-    while (charCount < WORD - 1 && (c = getchar()) != '\n' && c != '\t' && c != ' ')
-    { // getting word char by char
+    while (charCount < WORD - 1 && (c = getchar()) != '\n' && c != '\t' && c != ' ') // get char by char until space, tab or new line. keep the last char for \0
+    {                                                                                // getting word char by char
         *p++ = c;
         charCount++;
     }
-    return charCount;
+    return charCount + 1; // +1 for \0 at the end of the string
 };
 
 int getLine(char s[])
@@ -129,19 +129,18 @@ int getLine(char s[])
     char *p = s;
     int charCount = 0;
     char c;
-    while (charCount < LINE - 1 && (c = getchar()) != '\n')
-    { // getting line char by char
+    while (charCount < LINE - 1 && (c = getchar()) != '\n') // get char by char until new line. keep the last char for \0
+    {                                                       // getting line char by char
         *p++ = c;
         charCount++;
     }
-    return charCount;
+    return charCount + 1; // +1 for \0 at the end of the string
 };
 
-void print(char *arr, int len)
+void print(char *arr)
 {
     char *p = arr;
-    char *r = arr + len;
-    while (p != r)
+    while (*p) // print char by char until \0
     {
         printf("%c", *p);
         p++;
